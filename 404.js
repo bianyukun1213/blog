1d340fd8e8886e6cae97eca87f6d57b00f064171
Hollis <bianyukun1213@outlook.com>
2024-01-10 13:26:01 +0800

    原神，启动！

---
+++ 404.js
@@ -0,0 +1,66 @@
+function updatePageTitle() {
+    const lang = document.getElementsByTagName('html')[0].lang;
+    if (lang === 'zh-CN')
+        document.title = '页面不存在';
+    else
+        document.title = 'Page Not Found';
+}
+function updateNotFoundTitle() {
+    const lang = document.getElementsByTagName('html')[0].lang;
+    if (lang === 'zh-CN')
+        document.getElementById('not-found-title').innerText = '页面不存在';
+    else
+        document.getElementById('not-found-title').innerText = 'Page Not Found';
+}
+function updatePossibleLinkTip() {
+    let excluded = [];
+    try {
+        excluded = JSON.parse(decodeURIComponent(window.location.hash.replace('#excluded=', '')));
+    } catch (error) {
+        excluded = [];
+    }
+    // 把自身 url 排除。
+    excluded.push(window.location.origin + window.location.pathname);
+    const lang = document.getElementsByTagName('html')[0].lang;
+    const fromWhichSite = window.location.pathname.split('/')[1];
+    let possibleLinks = [];
+    if (fromWhichSite === 'zh-CN') {
+        const possibleLink = window.location.origin + window.location.pathname.replace(fromWhichSite, 'en');
+        if (!excluded.includes(possibleLink))
+            possibleLinks.push(possibleLink);
+    }
+    else if (fromWhichSite === 'en') {
+        const possibleLink = window.location.origin + window.location.pathname.replace(fromWhichSite, 'zh-CN');
+        if (!excluded.includes(possibleLink))
+            possibleLinks.push(possibleLink);
+    }
+    // 这种情况是匹配不上页面语言，比如旧的链接 pathname 开头不是 /zh-CN 或 /en。
+    else {
+        const possibleLinkZhCn = `${window.location.origin}/zh-CN${window.location.pathname}`;
+        const possibleLinkEn = `${window.location.origin}/en${window.location.pathname}`;
+        if (!excluded.includes(possibleLinkZhCn))
+            possibleLinks.push(possibleLinkZhCn);
+        if (!excluded.includes(possibleLinkEn))
+            possibleLinks.push(possibleLinkEn);
+    }
+    if (possibleLinks.length > 0) {
+        if (lang === 'zh-CN')
+            document.getElementById('possible-link-tip').innerText = '您还可以尝试访问以下可能的链接：';
+        else
+            document.getElementById('possible-link-tip').innerText = 'You can also try visiting the following possible links:';
+        for (const possibleLink of possibleLinks) {
+            const li = document.createElement('li');
+            li.innerHTML = `<a href="${possibleLink}#excluded=${encodeURIComponent(JSON.stringify(excluded))}">${possibleLink}</a>`;
+            document.getElementById('possible-links').appendChild(li);
+        }
+    }
+}
+let lang = '';
+if (navigator.language.startsWith('zh'))
+    lang = 'zh-CN';
+else
+    lang = 'en';
+document.getElementsByTagName('html')[0].lang = lang;
+updatePageTitle();
+updateNotFoundTitle();
+updatePossibleLinkTip();
---
+++ index.js
@@ -0,0 +1,29 @@
+function updatePageTitle() {
+    const lang = document.getElementsByTagName('html')[0].lang;
+    if (lang === 'zh-CN')
+        document.title = '他的第二人生';
+    else
+        document.title = 'His 2nd Life';
+}
+function updateTip(seconds) {
+    const lang = document.getElementsByTagName('html')[0].lang;
+    if (lang === 'zh-CN')
+        document.getElementById('jump-tip').innerText = `${seconds} 秒后自动跳转到中文站……`;
+    else
+        document.getElementById('jump-tip').innerText = `Automatically jump to English site after ${seconds}s…`;
+}
+let lang = '';
+if (navigator.language.startsWith('zh'))
+    lang = 'zh-CN';
+else
+    lang = 'en';
+document.getElementsByTagName('html')[0].lang = lang;
+updatePageTitle();
+let secondsRemain = 5;
+setInterval(() => {
+    if (secondsRemain <= 0) {
+        window.location.pathname = `/${lang}`;
+        return; // 距离跳转还有短暂的一点时间，不再更新秒数。
+    }
+    updateTip(secondsRemain--);
+}, 1000);
