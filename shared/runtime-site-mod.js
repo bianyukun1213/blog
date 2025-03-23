7e5bfce369f67a8271ef6341298f0e54e1254990
Hollis <bianyukun1213@outlook.com>
2025-03-23 14:03:40 +0800

    update

--- shared/runtime-site-mod.js
+++ shared/runtime-site-mod.js
@@ -1,7 +1,22 @@
 // 修复移动端网易云音乐外链。
-if (clientUtils.isMobileUserAgent(navigator.userAgent)) {
-    [...document.getElementsByTagName('iframe')].forEach(element => {
-        if (element.src.includes('music.163.com/'))
-            element.src = element.src.replace('music.163.com/', 'music.163.com/m/');
-    });
+function fixNetEaseMusic() {
+    if (clientUtils.isMobileUserAgent(navigator.userAgent)) {
+        [...document.getElementsByTagName('iframe')].forEach(element => {
+            if (element.src.includes('music.163.com/'))
+                element.src = element.src.replace('music.163.com/', 'music.163.com/m/');
+        });
+    }
 }
+
+function domContentLoadedHandler(eDomContentLoaded) {
+    fixNetEaseMusic();
+}
+
+if (document.readyState !== 'loading')
+    domContentLoadedHandler();
+else
+    document.addEventListener('DOMContentLoaded', domContentLoadedHandler);
+
+window.addEventListener('hexo-blog-decrypt', () => {
+    fixNetEaseMusic();
+});
