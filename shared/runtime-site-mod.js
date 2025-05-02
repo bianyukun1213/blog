44fe7f3c14a778c6259f78dac89e15b289214383
Hollis <bianyukun1213@outlook.com>
2025-05-02 22:17:06 +0800

    update

--- shared/runtime-site-mod.js
+++ shared/runtime-site-mod.js
@@ -1,3 +1,13 @@
+function fixPathname(pathnameIn) {
+    let fixedPathname = pathnameIn;
+    if (!fixedPathname.endsWith('/') && !fixedPathname.endsWith('.html'))
+        fixedPathname += '.html'; // 为结尾不带 / 或 .html 的页面添加 .html。
+    if (fixedPathname.endsWith('index.html'))
+        // 页面总是写在 .html 文件中，但在浏览器中访问时以 / 结尾。
+        fixedPathname = fixedPathname.replace('index.html', '');
+    return fixedPathname;
+}
+
 // 修复移动端网易云音乐外链。
 function fixNetEaseMusic() {
     if (clientUtils.isMobileUserAgent(navigator.userAgent)) {
@@ -11,6 +21,11 @@ function fixNetEaseMusic() {
 function domContentLoadedHandler(eDomContentLoaded) {
     fixNetEaseMusic();
 }
+
+const pathname = window.location.pathname;
+const fixedPathname = fixPathname(pathname);
+if (pathname !== fixedPathname)
+    window.location.replace(window.location.origin + fixedPathname); // 跳转。
 
 if (document.readyState !== 'loading')
     domContentLoadedHandler();
