dde7f1e2500853e2259de7d8918d4837d89a27c8
Hollis <bianyukun1213@outlook.com>
2025-06-04 11:41:42 +0800

    update

--- shared/runtime-site-mod.js
+++ shared/runtime-site-mod.js
@@ -8,14 +8,57 @@ function fixPathname(pathnameIn) {
     return fixedPathname;
 }
 
-// 修复移动端网易云音乐外链。
-function fixNetEaseMusic() {
-    if (clientUtils.isMobileUserAgent(navigator.userAgent)) {
-        [...document.getElementsByTagName('iframe')].forEach(element => {
-            if (element.src.includes('music.163.com/'))
-                element.src = element.src.replace('music.163.com/', 'music.163.com/m/');
+// 参考 https://github.com/YunYouJun/hexo-tag-common/blob/main/js/index.js
+// 额外添加 tabindex、role 与按键监听。
+function registerTabsTag() {
+    // Binding `nav-tabs` & `tab-content` by real time permalink changing.
+    document.querySelectorAll(".tabs ul.nav-tabs .tab").forEach((element) => {
+        const tabClick = () => {
+            // Prevent selected tab to select again.
+            if (element.classList.contains("active")) return;
+            // Add & Remove active class on `nav-tabs` & `tab-content`.
+            [...element.parentNode.children].forEach((target) => {
+                target.classList.toggle("active", target === element);
+                if (target.classList.contains("active"))
+                    target.removeAttribute("tabindex");
+                else
+                    target.setAttribute("tabindex", "0");
+            });
+            // https://stackoverflow.com/questions/20306204/using-queryselector-with-ids-that-are-numbers
+            const tActive = document.getElementById(
+                element.querySelector("a").dataset.target
+            );
+            [...tActive.parentNode.children].forEach((target) => {
+                target.classList.toggle("active", target === tActive);
+                if (target.classList.contains("active"))
+                    target.removeAttribute("tabindex");
+                else
+                    target.setAttribute("tabindex", "0");
+            });
+            // Trigger event
+            tActive.dispatchEvent(
+                new Event("tabs:click", {
+                    bubbles: true,
+                })
+            );
+        };
+        element.role = "button";
+        if (element.classList.contains("active"))
+            element.removeAttribute("tabindex");
+        else
+            element.setAttribute("tabindex", "0");
+        element.addEventListener("click", function (e) {
+            e.preventDefault();
+            tabClick(e);
         });
-    }
+        element.addEventListener("keydown", function (e) {
+            if (e.code === "Enter" || e.code === "Space") {
+                e.preventDefault();
+                tabClick(e);
+            }
+        });
+    });
+    window.dispatchEvent(new Event("tabs:register"));
 }
 
 function addAriaRoleToCollapseControlTag() {
@@ -31,52 +74,20 @@ function addAriaRoleToCollapseControlTag() {
     }
 }
 
-// 参考 https://github.com/YunYouJun/hexo-tag-common/blob/main/js/index.js
-// 额外添加 tabindex、role 与按键监听。
-function registerTabsTag() {
-    // Binding `nav-tabs` & `tab-content` by real time permalink changing.
-    document.querySelectorAll(".tabs ul.nav-tabs .tab").forEach((element) => {
-        const tabClick = () => {
-            // Prevent selected tab to select again.
-            if (element.classList.contains("active")) return;
-            // Add & Remove active class on `nav-tabs` & `tab-content`.
-            [...element.parentNode.children].forEach((target) => {
-                target.classList.toggle("active", target === element);
-            });
-            // https://stackoverflow.com/questions/20306204/using-queryselector-with-ids-that-are-numbers
-            const tActive = document.getElementById(
-                element.querySelector("a").dataset.target
-            );
-            [...tActive.parentNode.children].forEach((target) => {
-                target.classList.toggle("active", target === tActive);
-            });
-            // Trigger event
-            tActive.dispatchEvent(
-                new Event("tabs:click", {
-                    bubbles: true,
-                })
-            );
-        };
-        element.role = 'button';
-        element.setAttribute('tabindex', '0');
-        element.addEventListener("click", function (e) {
-            e.preventDefault();
-            tabClick(e);
+// 修复移动端网易云音乐外链。
+function fixNetEaseMusic() {
+    if (clientUtils.isMobileUserAgent(navigator.userAgent)) {
+        [...document.getElementsByTagName('iframe')].forEach(element => {
+            if (element.src.includes('music.163.com/'))
+                element.src = element.src.replace('music.163.com/', 'music.163.com/m/');
         });
-        element.addEventListener("keydown", function (e) {
-            if (e.code === "Enter" || e.code === "Space") {
-                e.preventDefault();
-                tabClick(e);
-            }
-        });
-    });
-    window.dispatchEvent(new Event("tabs:register"));
+    }
 }
 
 function domContentLoadedHandler(eDomContentLoaded) {
-    fixNetEaseMusic();
-    addAriaRoleToCollapseControlTag();
     registerTabsTag();
+    addAriaRoleToCollapseControlTag();
+    fixNetEaseMusic();
 }
 
 const pathname = window.location.pathname;
