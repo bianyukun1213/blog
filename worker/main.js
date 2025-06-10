3f9aab6569ace33cf135c1e90da833ef75765d74
Hollis <bianyukun1213@outlook.com>
2025-06-10 18:31:30 +0800

    update

--- worker/main.js
+++ worker/main.js
@@ -3,24 +3,28 @@
 import { WorkerEntrypoint } from "cloudflare:workers";
 
 export default class extends WorkerEntrypoint {
-  async fetch(request) {
-    // You can perform checks before fetching assets
-    const user = await checkIfRequestIsAuthenticated(request);
+    async fetch(request) {
+        // // You can perform checks before fetching assets
+        // const user = await checkIfRequestIsAuthenticated(request);
 
-    if (!user) {
-      return new Response("Unauthorized", { status: 401 });
+        // if (!user) {
+        //   return new Response("Unauthorized", { status: 401 });
+        // }
+
+        if (request.url.endsWith('/'))
+            return await this.env.ASSETS.fetch(request.url + 'index.html');
+        return await this.env.ASSETS.fetch(request);
+
+        // // You can then just fetch the assets as normal, or you could pass in a custom Request object here if you wanted to fetch some other specific asset
+        // const assetResponse = await this.env.ASSETS.fetch(request);
+
+        // // You can return static asset response as-is, or you can transform them with something like HTMLRewriter
+        // return new HTMLRewriter()
+        //     .on("#user", {
+        //         element(element) {
+        //             element.setInnerContent(JSON.stringify({ name: user.name }));
+        //         },
+        //     })
+        //     .transform(assetResponse);
     }
-
-    // You can then just fetch the assets as normal, or you could pass in a custom Request object here if you wanted to fetch some other specific asset
-    const assetResponse = await this.env.ASSETS.fetch(request);
-
-    // You can return static asset response as-is, or you can transform them with something like HTMLRewriter
-    return new HTMLRewriter()
-      .on("#user", {
-        element(element) {
-          element.setInnerContent(JSON.stringify({ name: user.name }));
-        },
-      })
-      .transform(assetResponse);
-  }
 }
