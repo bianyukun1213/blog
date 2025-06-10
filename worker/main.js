3fe8370fb35be6d852bb41fae25a62b0cede9adb
Hollis <bianyukun1213@outlook.com>
2025-06-10 19:44:25 +0800

    update

--- worker/main.js
+++ worker/main.js
@@ -4,20 +4,22 @@ import { WorkerEntrypoint } from "cloudflare:workers";
 
 export default class extends WorkerEntrypoint {
     async fetch(request) {
+        let redirect = false;
         const parsed = new URL(request.url);
         const { pathname, search, hash } = parsed;
         if (pathname.endsWith('/')) {
             parsed.pathname = pathname + 'index.html';
-            return await this.env.ASSETS.fetch(parsed.toString());
         } else {
             const segments = pathname.split('/');
             const lastSegment = segments.pop() || '';
             if (lastSegment && !lastSegment.includes('.')) {
                 parsed.pathname = segments.join('/') + '/' + lastSegment + '.html';
-                return Response.redirect(parsed.toString(), 301);
-            } else {
-                return await this.env.ASSETS.fetch(parsed.toString());
+                redirect = true;
             }
         }
+        if (redirect)
+            return Response.redirect(parsed.toString(), 301);
+        else
+            return await this.env.ASSETS.fetch(parsed.toString());
     }
 }
