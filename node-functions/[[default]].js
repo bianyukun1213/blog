a8db05735b2970cc377fa4a1396daaa6a73d40f8
Hollis <bianyukun1213@outlook.com>
2025-09-24 15:53:34 +0800

    update

--- node-functions/[[default]].js
+++ node-functions/[[default]].js
@@ -2,10 +2,9 @@ import express from "express";
 
 const app = express();
 
-app.use(async (req, res, next) => {
+app.use((req, res, next) => {
   let redirect = false;
-  const url = new URL(req.url, `http://${req.headers.host}`);
-
+  const url = new URL(req.url, `https://${req.headers.host}`);
   const { pathname } = url;
 
   if (pathname.endsWith('/index.html')) {
@@ -24,19 +23,9 @@ app.use(async (req, res, next) => {
 
   if (redirect) {
     res.redirect(301, url.toString());
-    return;
+  } else {
+    next(); // 交给 EdgeOne Pages 的静态资源层处理
   }
-
-  // 调用 EdgeOne 提供的 ASSETS
-  const assetResponse = await req.context.env.ASSETS.fetch(url.toString());
-
-  // 转发 headers 和 body
-  res.status(assetResponse.status);
-  for (const [k, v] of assetResponse.headers.entries()) {
-    res.setHeader(k, v);
-  }
-  res.send(Buffer.from(await assetResponse.arrayBuffer()));
 });
 
-// 必须导出 app
 export default app;
