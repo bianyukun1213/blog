9895b0e3648acd6ae4afb388a7a953e8404bf333
Hollis <bianyukun1213@outlook.com>
2025-09-24 15:34:20 +0800

    update

--- node-functions/[[default]].js
+++ node-functions/[[default]].js
@@ -1,4 +1,8 @@
-export default async function (req, res, context) {
+import express from "express";
+
+const app = express();
+
+app.use(async (req, res, next) => {
   let redirect = false;
   const url = new URL(req.url, `http://${req.headers.host}`);
 
@@ -19,17 +23,20 @@ export default async function (req, res, context) {
   }
 
   if (redirect) {
-    // 301 重定向
-    res.writeHead(301, { Location: url.toString() });
-    res.end();
+    res.redirect(301, url.toString());
     return;
   }
 
-  // 调用静态资源（Pages 的 Assets）
-  const assetResponse = await context.env.ASSETS.fetch(url.toString());
+  // 调用 EdgeOne 提供的 ASSETS
+  const assetResponse = await req.context.env.ASSETS.fetch(url.toString());
 
   // 转发 headers 和 body
-  res.writeHead(assetResponse.status, Object.fromEntries(assetResponse.headers.entries()));
-  const buffer = Buffer.from(await assetResponse.arrayBuffer());
-  res.end(buffer);
-}
+  res.status(assetResponse.status);
+  for (const [k, v] of assetResponse.headers.entries()) {
+    res.setHeader(k, v);
+  }
+  res.send(Buffer.from(await assetResponse.arrayBuffer()));
+});
+
+// 必须导出 app
+export default app;
